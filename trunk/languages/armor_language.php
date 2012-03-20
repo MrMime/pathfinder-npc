6a7787f2fd0bd98bfec73274235755ef62c73ebc
amadei@gmail.com <amadei@gmail.com@60d2af27-d6b6-e899-a66a-80af30a56ce0>
2012-03-20 10:00:20 +0000

    tradotte le armature

--- trunk/languages/armor_language.php
+++ trunk/languages/armor_language.php
@@ -5,11 +5,63 @@
 	$pfArmor['eng']['medium'] 			= 'medium';
 	$pfArmor['eng']['heavy'] 			= 'heavy';
 	
+	$pfArmor['eng']['nake'] 			= 'nake';
+	$pfArmor['eng']['padded'] 			= 'padded';
+	$pfArmor['eng']['quiltedcloth'] 	= 'quilted cloth';
+	$pfArmor['eng']['leather'] 			= 'leather';
+	$pfArmor['eng']['rosewood'] 		= 'rose wood';
+	$pfArmor['eng']['leafarmor'] 		= 'leaf armor';
+	$pfArmor['eng']['paradearmor'] 		= 'parade armor';
+	$pfArmor['eng']['wooden'] 			= 'wooden';
+	$pfArmor['eng']['chainshirt'] 		= 'chains hirt';
+	$pfArmor['eng']['hideshirt'] 		= 'hide hirt';
+	
+	$pfArmor['eng']['armoredcoat'] 		= 'armoredcoat';
+	$pfArmor['eng']['hide'] 			= 'hide';
+	$pfArmor['eng']['scalemail'] 		= 'scalemail';
+	$pfArmor['eng']['chainmail'] 		= 'chainmail';
+	$pfArmor['eng']['breastplate'] 		= 'breastplate';
+	$pfArmor['eng']['braastplateagile'] = 'braastplateagile';
+	
+	$pfArmor['eng']['splintmail'] 		= 'splintmail';
+	$pfArmor['eng']['bandedmail'] 		= 'bandedmail';
+	$pfArmor['eng']['fieldplate'] 		= 'fieldplate';
+	$pfArmor['eng']['halfplate'] 		= 'halfplate';
+	$pfArmor['eng']['fullplate'] 		= 'fullplate';
+	$pfArmor['eng']['hellknightplate'] 	= 'hellknightplate';
+	$pfArmor['eng']['stoneplate'] 		= 'stoneplate';
+	
 	
 	/************   ITA  **************************/ 
 	$pfArmor['ita']['light'] 			= 'leggere';
 	$pfArmor['ita']['medium'] 			= 'medie';
 	$pfArmor['ita']['heavy'] 			= 'pesanti';
+	
+	$pfArmor['ita']['nake'] 			= 'nudo';
+	$pfArmor['ita']['padded'] 			= 'imbottita';
+	$pfArmor['ita']['quiltedcloth'] 	= 'quilted cloth';
+	$pfArmor['ita']['leather'] 			= 'cuoio';
+	$pfArmor['ita']['rosewood'] 		= 'rose wood';
+	$pfArmor['ita']['leafarmor'] 		= 'leaf armor';
+	$pfArmor['ita']['paradearmor'] 		= 'armatura da parata';
+	$pfArmor['ita']['wooden'] 			= 'wooden';
+	$pfArmor['ita']['chainshirt'] 		= 'giacco di maglia';
+	$pfArmor['ita']['hideshirt'] 		= 'hide hirt';
+	
+	$pfArmor['ita']['armoredcoat'] 		= 'armoredcoat';
+	$pfArmor['ita']['hide'] 			= 'pelle';
+	$pfArmor['ita']['scalemail'] 		= 'corazza scaglie';
+	$pfArmor['ita']['chainmail'] 		= 'cotta di maglia';
+	$pfArmor['ita']['breastplate'] 		= 'corazza di piastre';
+	$pfArmor['ita']['braastplateagile'] = 'corazza di piastre agile';
+	
+	$pfArmor['ita']['splintmail'] 		= 'corazza di strisce';
+	$pfArmor['ita']['bandedmail'] 		= 'corazza di bande';
+	$pfArmor['ita']['fieldplate'] 		= 'fieldplate';
+	$pfArmor['ita']['halfplate'] 		= 'mezza armatura';
+	$pfArmor['ita']['fullplate'] 		= 'armatura completa';
+	$pfArmor['ita']['hellknightplate'] 	= 'hellknightplate';
+	$pfArmor['ita']['stoneplate'] 		= 'stoneplate';
 	
 	$GLOBALS['pfArmor'] = $pfArmor;
 
--- trunk/languages/shield_language.php
+++ trunk/languages/shield_language.php
@@ -5,11 +5,38 @@
 	$pfShield['eng']['medium'] 			= 'medium';
 	$pfShield['eng']['heavy'] 			= 'heavy';
 	
+	$pfShield['eng']['blucker'] 		= 'blucker';	
+	$pfShield['eng']['klar'] 			= 'klar';
+	$pfShield['eng']['maduleather'] 	= 'maduleather';
+	$pfShield['eng']['madusteel'] 		= 'madusteel';
+	$pfShield['eng']['lightwooden'] 	= 'light wooden';
+	$pfShield['eng']['lightwoodenquickdraw'] 		= '^ quick draw';
+	$pfShield['eng']['lightsteel'] 		= 'light steel';
+	$pfShield['eng']['lightsteelquickdraw'] 		= '^ quick draw';
+	
+	$pfShield['eng']['heavywooden'] 	= 'heavy wooden';
+	$pfShield['eng']['heavysteel'] 		= 'heavy steel';
+	$pfShield['eng']['tower'] 			= 'tower';
+	
 	
 	/************   ITA  **************************/ 
 	$pfShield['ita']['light'] 			= 'leggere';
 	$pfShield['ita']['medium'] 			= 'medie';
 	$pfShield['ita']['heavy'] 			= 'pesanti';
+	
+	$pfShield['ita']['blucker'] 		= 'blucker';	
+	$pfShield['ita']['klar'] 			= 'klar';
+	$pfShield['ita']['maduleather'] 	= 'maduleather';
+	$pfShield['ita']['madusteel'] 		= 'madusteel';
+	$pfShield['ita']['lightwooden'] 	= 'leggero di legno';
+	$pfShield['ita']['lightwoodenquickdraw'] 		= '^ estrazione rapida';
+	$pfShield['ita']['lightsteel'] 		= 'leggero di metallo';
+	$pfShield['ita']['lightsteelquickdraw'] 		= '^ estrazione rapida';
+	
+	$pfShield['ita']['heavywooden'] 		= 'pesante di legno';
+	$pfShield['ita']['heavysteel'] 		= 'pesante di metallo';
+	$pfShield['ita']['tower'] 		= 'torre';
+		
 	
 	$GLOBALS['pfShield'] = $pfShield;
 
